92bd944d49b154999f1245e28b7129c3e99cabca
John Lockwood <John@CodeSolid.com>
2016-06-11 21:57:13 -0700

    Angular 2 getting started tutorial, complete and running

---
+++ app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>My First Angular 2 App</h1><p>Consisting of the original stuff and this awesome John-added paragraph.</p>'
+})
+export class AppComponent { }
