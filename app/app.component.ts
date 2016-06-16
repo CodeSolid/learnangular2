81d9c78e3e7b896aee876cfe0486bd8f030b4907
John Lockwood <John@CodeSolid.com>
2016-06-16 05:55:26 -0700

    Tour of Heroes, end of Multiple Components section

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,28 +1,21 @@
 import { Component } from '@angular/core';
-
-export class Hero {
-	id: number;
-	name: string;
-};
+import { HeroDetailComponent } from './hero-detail.component';
+import { Hero } from './hero';
 
 @Component({  
   selector: 'my-app',
   template: `
-  	<h2>My Heroes</h2>
+		<h1>{{title}}</h1>
+		<h2>My Heroes</h2>
 		<ul class="heroes">
-  			<li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
-  			 	<span class="badge">{{hero.id}}</span> {{hero.name}}
-  			</li>
+		<li *ngFor="let hero of heroes"
+		[class.selected]="hero === selectedHero"
+		(click)="onSelect(hero)">
+		<span class="badge">{{hero.id}}</span> {{hero.name}}
+		</li>
 		</ul>
-	<div *ngIf="selectedHero">
-		<h2>{{selectedHero.name}} details!</h2>
-			<div><label>id: </label>{{selectedHero.id}}</div>
-		<div>
-			<label>name: </label>
-			<input [(ngModel)]="selectedHero.name" placeholder="name"/>
-		</div>
-	</div>
-		`,
+		<my-hero-detail [hero]="selectedHero"></my-hero-detail>
+	`,
 	styles:[`
 	  .selected {
 	    background-color: #CFD8DC !important; 
@@ -72,11 +65,7 @@ export class Hero {
 	    border-radius: 4px 0 0 4px;
 	  }
 	`],
-	
-
-
-
-
+	directives: [HeroDetailComponent],
 })
 
 export class AppComponent { 
